4c63b56b1dcad775a84cd21542acb7a093f48ce2
TonyIIO <149205637+TonyIIO@users.noreply.github.com>
2024-05-16 15:10:28 +0300

    lib3step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!')
+import readlineSync from 'node_modules/readline-sync/lib/readline-sync.js';
+
+/* import readlineSync from './lib/readline-sync.js'; 
+
+console.log(`May I have your name? ${name}`)*/
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`)
