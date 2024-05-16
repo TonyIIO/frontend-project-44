0fd51a194c1eca2252dd3da23eeb56bdde30c84e
TonyIIO <149205637+TonyIIO@users.noreply.github.com>
2024-05-16 15:23:16 +0300

    working 3 st

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!')
-import readlineSync from 'node_modules/readline-sync/lib/readline-sync.js';
-
-/* import readlineSync from './lib/readline-sync.js'; 
-
-console.log(`May I have your name? ${name}`)*/
+import readlineSync from 'readline-sync';
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`)
