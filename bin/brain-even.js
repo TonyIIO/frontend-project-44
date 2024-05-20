43a5abfbf9ac7661a7e064cfaef8cf982c944ed1
TonyIIO <149205637+TonyIIO@users.noreply.github.com>
2024-05-20 17:24:29 +0300

    fi[2 5st

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,19 +2,18 @@ import readlineSync from 'readline-sync';
 import { name } from '../scr/cli';
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const randomNumb =  Math.floor(Math.random() * 101);
-
-const match = () => {
+const questions = () => {
   for (let i = 0; i < 3; i += 1) {
     console.log(`Question: ${randomNumb}!`);
     const answer = readlineSync.question('Your answer:');
     if (answer === 'yes' && randomNumb % 2 === 0) {
       console.log ('Correct!');
-    };
-    if (answer === 'no' && randomNumb % 2 !== 0) {
+    } else if (answer === 'no' && randomNumb % 2 !== 0) {
       console.log ('Correct!');
-    };
-    return "Let's try again, Bill!";
+    } else { 
+      return "Let's try again, Bill!";
   };
   console.log (`Congratulations, ${name}!`);
   };
-console.log (match);
+};
+console.log (questions());
