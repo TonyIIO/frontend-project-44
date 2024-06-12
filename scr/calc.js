2dd58810ade286d635e4c1e40403a22637df67e3
TonyIIO <149205637+TonyIIO@users.noreply.github.com>
2024-06-12 11:44:02 +0300

    fixlint8

--- scr/calc.js
+++ scr/calc.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
-import { name } from '../scr/cli.js';
-import { getRandomInt } from '../scr/index.js';
+import { name } from './cli.js';
+import { getRandomInt } from './index.js';
 
-export const calc = () => {
+const calc = () => {
   console.log(`Hello, ${name}!`);
-  console.log ('What is the result of the expression?');
+  console.log('What is the result of the expression?');
   for (let i = 0; i < 3; i += 1) {
     const oneRandNumb = getRandomInt(1, 10);
     const twoRandNumb = getRandomInt(1, 10);
@@ -12,23 +12,25 @@ export const calc = () => {
     const randSign = sign[getRandomInt(0, 2)];
     let resultAB = 0;
     switch (randSign) {
-      case '+': 
-      resultAB = oneRandNumb + twoRandNumb;
-      break;
-      case '*': 
-      resultAB = oneRandNumb * twoRandNumb;
-      break;
-      case '-': 
-      resultAB = oneRandNumb - twoRandNumb;
-      break;  
-    };    
-    console.log (`Question: ${oneRandNumb} ${randSign} ${twoRandNumb}`);
+      case '+':
+        resultAB = oneRandNumb + twoRandNumb;
+        break;
+      case '*':
+        resultAB = oneRandNumb * twoRandNumb;
+        break;
+      case '-':
+        resultAB = oneRandNumb - twoRandNumb;
+        break;
+    }
+    console.log(`Question: ${oneRandNumb} ${randSign} ${twoRandNumb}`);
     const answer = readlineSync.question('Your answer:');
-      if (Number (answer) === resultAB) {
-      console.log ('Correct!');
+    if (Number(answer) === resultAB) {
+      console.log('Correct!');
     } else {
-      return console.log (`${answer} is wrong answer ;(. Correct answer was ${resultAB}. Let's try again, ${name}!`);       
-      };      
-  };
-  console.log (`Congratulations, ${name}!`);
+      return console.log(`${answer} is wrong answer ;(. Correct answer was ${resultAB}. Let's try again, ${name}!`);       
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
+
+export default calc;
--- scr/index.js
+++ scr/index.js
@@ -11,7 +11,7 @@ export const getRandomInt = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
- 
+
 export const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
@@ -19,12 +19,11 @@ export const isPrime = (num) => {
   return num !== 1;
 };
 
-
-export const  getProgression = (progressionStep, progressionBase, progressionLength) => {
+export const getProgression = (progressionStep, progressionBase, progressionLength) => {
   const arrProgression = [];
 
   for (let i = 0; i < progressionLength; i += 1) {
     arrProgression[i] = progressionBase + i * progressionStep;      
   }
-  return arrProgression;
+ return arrProgression;
 };
