2f7259093eb108b03418bdffa95771c4783658d0
TonyIIO <149205637+TonyIIO@users.noreply.github.com>
2024-06-12 11:54:07 +0300

    9fix games

--- scr/cli.js
+++ scr/cli.js
@@ -1,6 +1,4 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question(`Welcome to the Brain Games!
+export const name = readlineSync.question(`Welcome to the Brain Games!
 May I have your name?`);
-
-export default name;
